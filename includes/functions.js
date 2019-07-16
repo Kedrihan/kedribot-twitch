9ec18a32d0beb8a3450eb9ab57135eec7c7f64af
Corentin <c.faure1996@gmail.com>
2019-07-16 21:52:52 +0200

    Update

--- includes/functions.js
+++ includes/functions.js
@@ -2,7 +2,7 @@ let connection = require("./dbHandler.js");
 
 module.exports = {
     addToDb: function(message, context) {
-        let sql = "INSERT INTO messages (auteur, message) VALUES (?, ?, ?)";
+        let sql = "INSERT INTO messages (auteur, message) VALUES (?, ?)";
         connection.query(sql, [context.username, message], (err) => {
       if (err) console.log(err);
     });
