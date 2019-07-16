baad75f4569826adaae19844eb37e67d106cd669
Corentin <c.faure1996@gmail.com>
2019-07-16 21:41:43 +0200

    Update

--- includes/functions.js
+++ includes/functions.js
@@ -2,7 +2,7 @@ let connection = require("./dbHandler.js");
 
 module.exports = {
     addToDb: function(message, context) {
-        let sql = "INSERT INTO twitch_chat (auteur, message, date_heure) VALUES (?, ?, ?)";
+        let sql = "INSERT INTO messages (auteur, message, date_heure) VALUES (?, ?, ?)";
         connection.query(sql, [context.username, message, Date.now()], (err) => {
       if (err) console.log(err);
     });
